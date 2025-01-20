8ae91e0e743af695c20552521448b87c752b938e
vrushitarnw2266 <vrushita2366@gmail.com>
2025-01-20 13:46:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,99 @@
+let a = 3
+let b = 3
+let c = (a+b)
+console.log(c)
+
+let d = 20
+let e = 3
+let f = (d-e)
+console.log(f)
+
+let g = 13
+let h = 3
+let i = (g*h)
+console.log(i)
+
+let j = 25
+let k = 3
+let l = (j/k)
+console.log(l)
+
+let m = 25
+let n = 3
+let o =(m%n)
+console.log(o)
+
+let p = 23
+
+++p
+console.log(p)
+
+let q = 24
+q++
+console.log(q)
+
+let r = 29
+--r
+console.log(r)
+
+let s = 29
+let ss = s--
+console.log(s)
+console.log(ss)
+
+let t = 23
+let u = 3
+let v = (t**u)
+console.log(v)
+
+let w = 2
+let x = 3
+let y = (w=x)
+console.log(y)
+
+let A = "2"
+let B = 2
+let C =(A==B)
+console.log(C)
+
+let D = 2
+let E = "4"
+let F = (D==E)
+console.log(F)
+
+let G = 3
+let H = 3
+let I = (G!=H)
+console.log(I)
+
+let J = 2
+let K = 3
+let L =(J!=K)
+console.log(L)
+
+let M = 3
+let N = "3"
+let O = (M!=N)
+console.log(O)
+
+let P = 2 
+let Q = "4"
+let R = (P!=Q)
+console.log(R)
+
+let S = 2
+let T = 2
+let U = (S===T)
+console.log(U)
+
+let V = 2
+let W = "2"
+let X = (V===W)
+console.log(X)
+
+
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,56 @@
+let a = 2 
+let b = 2
+let c =(a!==b)
+console.log(c)
+
+let d = 2
+let e = "2"
+let f = (d!==e)
+console.log(f)
+
+
+let g = 3
+let h = 2
+let i =(g===h)
+console.log(i)
+
+let j = 2
+let k = 3
+let l = (j!==k)
+console.log(l)
+
+let m = 3
+let n = "2"
+let o = (m!==n)
+console.log(o)
+
+let p = 3
+let q = 3
+let r = (p>=q)
+console.log(r)
+
+let s = 3
+let t = 3
+let u = (s<=t)
+console.log(u)
+
+let v = 2
+let w = 3
+let x =(v>=w)
+console.log(x)
+
+let y = 6
+let z = 3
+let A = (y>=z)
+console.log(A)
+
+
+let B = 33
+let C = 3
+let D =(B<=C)
+console.log(D)
+
+let E = 34
+let F = 44
+let G = (E<=F)
+console.log(G)
